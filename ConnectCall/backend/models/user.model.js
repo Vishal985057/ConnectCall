740821894a534cfc9203afd56b21976adfa27505
Vishal985057 <159512784+Vishal985057@users.noreply.github.com>
2026-06-24 21:30:48 +0530

    Delete ConnectCall/backend directory

--- ConnectCall/backend/models/user.model.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const s = new mongoose.Schema({
-  name: { type: String, required: true },
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  token: String,
-});
-export const User = mongoose.model("User", s);
